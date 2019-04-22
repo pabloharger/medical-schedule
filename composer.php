640a8d3c8b2237940637bb46b01c0cec906c4992
Pablo Harger <pabloharger@Pablos-MacBook-Pro.local>
2019-04-22 13:19:40 +0100

    fix composer components addresses

--- composer.php
+++ composer.php
@@ -25,10 +25,14 @@ function upd($path, $file, $web){
     if (file_put_contents($path . DIRECTORY_SEPARATOR . $file, file_get_contents($web))) echo "File $file - downloaded\n\n";
 }
 
+function updMsg($comp){
+    echo "---> Updating $comp\n\n";
+}
+
 function update_assets() {
     $pathComponents = 'res' . DIRECTORY_SEPARATOR . 'site' . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR;
 
-    echo "Update Bootstrap\n";
+    updMsg("Bootstrap");
     /*
     $components_path = 'res' . DIRECTORY_SEPARATOR . 'site' . DIRECTORY_SEPARATOR . 'components';
 
@@ -39,49 +43,55 @@ function update_assets() {
     
     upd($path, 'bootstrap.min.css', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css');
     upd($path, 'bootstrap.min.css.map', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css.map');
-    upd($path, 'bootstrap.min.js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js');
-    upd($path, 'bootstrap.min.js.map', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js.map');
+    upd($path, 'bootstrap.min.js', 'https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js');
+    upd($path, 'bootstrap.min.js.map', 'https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js.map');
     upd($path, 'font-awesome.min.css', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
 
-    echo "Update FullCalendar\n";
+    updMsg("FullCalendar");
 
     $path = $pathComponents . 'fullcalendar';
     upd($path, 'fullcalendar.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css');
+    upd($path, 'fullcalendar.print.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.print.min.css');
     upd($path, 'fullcalendar.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js');
 
 
-    echo "Update FullCalendar\n";
+    updMsg("jQuery");
 
     $path = $pathComponents . 'jquery';
     upd($path, 'jquery.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js');
 
 
-    echo "Update Popper\n";
+    updMsg("Popper");
 
     $path = $pathComponents . 'popper';
-    upd($path, 'popper.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/esm/popper.min.js');
+    //upd($path, 'popper.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/popper.min.js');
+    upd($path, 'popper.min.js', 'https://unpkg.com/popper.js@1.15.0/dist/umd/popper.min.js');
 
 
-    echo "Update Select2\n";
+    updMsg("Select2");
 
     $path = $pathComponents . 'select2';
-    upd($path, 'select2.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css');
-    upd($path, 'select2.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js');
+    upd($path, 'select2.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css');
+    upd($path, 'select2.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js');
     
-    echo "************************ Shortcut is not suported ************************ \n";
+    echo "*** Shortcut is not suported *** \n\n";
 
 
-    echo "Update bootstrap-datetimepicker\n";
+    updMsg("bootstrap-datetimepicker");
     $path = $pathComponents . 'bootstrap-datetimepicker';
     upd($path, 'bootstrap-datetimepicker.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css');
     upd($path, 'bootstrap-datetimepicker.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js');
 
 
-    echo "Update jQuery-Impromptu\n";
+    updMsg("jQuery-Impromptu");
     $path = $pathComponents . 'jQuery-Impromptu';
-    upd($path, 'jquery-impromptu.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-impromptu/6.2.3/themes/base.min.css');
-    upd($path, 'jquery-impromptu.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-impromptu/6.2.3/jquery-impromptu.min.js');
+    upd($path, 'jquery-impromptu.min.css', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-impromptu/6.2.2/jquery-impromptu.min.css');
+    upd($path, 'jquery-impromptu.css', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-impromptu/6.2.2/jquery-impromptu.css');
+    upd($path, 'jquery-impromptu.min.js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-impromptu/6.2.2/jquery-impromptu.min.js');
+    upd($path, 'jquery-impromptu.js', 'https://cdnjs.cloudflare.com/ajax/libs/jquery-impromptu/6.2.2/jquery-impromptu.js');
     
+
+
     //file_put_contents($dir . DIRECTORY_SEPARATOR . 'bootstrap.min.js', file_get_contents('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js'));
 
 
