6c5d6f66f7da412b60df737fd5d93e3759036ef4
Pablo Rodrigo Harger <pabloharger@gmail.com>
2020-04-21 17:34:40 +0100

    Add backend API / i18n

--- tmp/views-cache/footer.045f17fa4d7cd1e4124c919a37d3e033.rtpl.php
+++
@@ -1,7 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><!-- Afterload Scripts -->
-<script src="/res/site/js/afterload.js"></script>
-
-<footer class="page-footer font-small">
-
-	</footer>
-</html>
